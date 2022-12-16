00a49531b46aaa732cd77632a277714f18e18ed8
fushenshen <hellofss@foxmail.com>
2022-12-16 16:28:05 +0800

    第二次课程

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <armadillo>
-int main() {
-  arma::fmat in_1(32, 32, arma::fill::ones);
-  arma::fmat in_2(32, 32, arma::fill::ones);
+#include <glog/logging.h>
 
-  arma::fmat out = in_1 + in_2;
-  std::cout << "rows " << out.n_rows << "\n";
-  std::cout << "cols " << out.n_cols << "\n";
-  std::cout << "value " << out.at(0) << "\n";
+int main() {
+  LOG(INFO) << "Kuiper Infer Course";
   return 0;
 }
