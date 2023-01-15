0d4722cd1621bc08e071d477984020c68f0e0e77
fushenshen <hellofss@foxmail.com>
2023-01-15 22:48:46 +0800

    第七节课程

---
+++ include/parser/parse_expression.hpp
@@ -0,0 +1,65 @@
+//
+// Created by fss on 22-12-1.
+//
+
+#ifndef KUIPER_INFER_INCLUDE_PARSER_PARSE_EXPRESSION_HPP_
+#define KUIPER_INFER_INCLUDE_PARSER_PARSE_EXPRESSION_HPP_
+#include <string>
+#include <utility>
+#include <vector>
+#include <memory>
+
+namespace kuiper_infer {
+
+enum class TokenType {
+  TokenUnknown = -1,
+  TokenInputNumber = 0,
+  TokenComma = 1,
+  TokenAdd = 2,
+  TokenMul = 3,
+  TokenLeftBracket = 4,
+  TokenRightBracket = 5,
+};
+
+struct Token {
+  TokenType token_type = TokenType::TokenUnknown;
+  int32_t start_pos = 0; //词语开始的位置
+  int32_t end_pos = 0; // 词语结束的位置
+  Token(TokenType token_type, int32_t start_pos, int32_t end_pos)
+      : token_type(token_type), start_pos(start_pos), end_pos(end_pos) {
+
+  }
+};
+
+struct TokenNode {
+  int32_t num_index = -1;
+  std::shared_ptr<TokenNode> left = nullptr;
+  std::shared_ptr<TokenNode> right = nullptr;
+  TokenNode(int32_t num_index, std::shared_ptr<TokenNode> left, std::shared_ptr<TokenNode> right);
+  TokenNode() = default;
+};
+
+// add(add(add(@0,@1),@1),add(@0,@2))
+class ExpressionParser {
+ public:
+  explicit ExpressionParser(std::string statement) : statement_(std::move(statement)) {
+
+  }
+
+  void Tokenizer(bool need_retoken = false);
+
+  std::shared_ptr<TokenNode> Generate();
+
+  const std::vector<Token> &tokens() const;
+
+  const std::vector<std::string> &token_strs() const;
+
+ private:
+  std::shared_ptr<TokenNode> Generate_(int32_t &index);
+  std::vector<Token> tokens_;
+  std::vector<std::string> token_strs_;
+  std::string statement_;
+};
+}
+
+#endif //KUIPER_INFER_INCLUDE_PARSER_PARSE_EXPRESSION_HPP_
---
+++ test/test_expression.cpp
@@ -0,0 +1,42 @@
+//
+// Created by fss on 23-1-15.
+//
+
+#include "parser/parse_expression.hpp"
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+
+#include "parser/parse_expression.hpp"
+
+static void ShowNodes(const std::shared_ptr<kuiper_infer::TokenNode> &node) {
+  if (!node) {
+    return;
+  }
+  ShowNodes(node->left);
+  if (node->num_index < 0) {
+    if (node->num_index == -int(kuiper_infer::TokenType::TokenAdd)) {
+      LOG(INFO) << "ADD";
+    } else if (node->num_index == -int(kuiper_infer::TokenType::TokenMul)) {
+      LOG(INFO) << "MUL";
+    }
+  } else {
+    LOG(INFO) << "NUM: " << node->num_index;
+  }
+  ShowNodes(node->right);
+}
+
+TEST(test_expression, expression1) {
+  using namespace kuiper_infer;
+  const std::string &statement = "add(mul(@0,@1),@2)";
+  ExpressionParser parser(statement);
+  const auto &node_tokens = parser.Generate();
+  ShowNodes(node_tokens);
+}
+
+TEST(test_expression, expression2) {
+  using namespace kuiper_infer;
+  const std::string &statement = "add(mul(@0,@1),mul(@2,@3))";
+  ExpressionParser parser(statement);
+  const auto &node_tokens = parser.Generate();
+  ShowNodes(node_tokens);
+}
