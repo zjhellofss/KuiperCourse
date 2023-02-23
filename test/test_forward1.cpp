85acee81e9706529febafb3ce0628c9cc99e9321
fushenshen <hellofss@foxmail.com>
2023-02-23 21:15:40 +0800

    第11节课程

--- test/test_forward1.cpp
+++ test/test_forward1.cpp
@@ -19,5 +19,39 @@ TEST(test_forward, forward1) {
     inputs.at(i) = std::make_shared<ftensor>(3, 224, 224);
     inputs.at(i)->Fill(1.f);
   }
-  const std::vector<sftensor>& outputs = graph.Forward(inputs, true);
+  const std::vector<sftensor> &outputs = graph.Forward(inputs, true);
+}
+
+TEST(test_forward, forward2) {
+  using namespace kuiper_infer;
+  const std::string &param_path = "tmp/test.pnnx.param";
+  const std::string &weight_path = "tmp/test.pnnx.bin";
+  RuntimeGraph graph(param_path, weight_path);
+  graph.Build("pnnx_input_0", "pnnx_output_0");
+  const auto &operators = graph.operators();
+  LOG(INFO) << "operator size: " << operators.size();
+  uint32_t batch_size = 1;
+  std::vector<sftensor> inputs(batch_size);
+  for (uint32_t i = 0; i < batch_size; ++i) {
+    inputs.at(i) = std::make_shared<ftensor>(1, 16, 16);
+    inputs.at(i)->Fill(1.f);
+  }
+  const std::vector<sftensor> &outputs = graph.Forward(inputs, true);
+}
+
+TEST(test_forward, forward3) {
+  using namespace kuiper_infer;
+  const std::string &param_path = "tmp/ten.pnnx.param";
+  const std::string &weight_path = "tmp/ten.pnnx.bin";
+  RuntimeGraph graph(param_path, weight_path);
+  graph.Build("pnnx_input_0", "pnnx_output_0");
+  const auto &operators = graph.operators();
+  LOG(INFO) << "operator size: " << operators.size();
+  uint32_t batch_size = 2;
+  std::vector<sftensor> inputs(batch_size);
+  for (uint32_t i = 0; i < batch_size; ++i) {
+    inputs.at(i) = std::make_shared<ftensor>(3, 128, 128);
+    inputs.at(i)->Fill(1.f);
+  }
+  const std::vector<sftensor> &outputs = graph.Forward(inputs, true);
 }
